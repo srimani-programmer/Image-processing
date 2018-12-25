80c80d01f43ad0a1b1d3a81e2fbf3f6fb469a20a
srimani-programmer <srimani.scholar@gmail.com>
2018-12-25 13:01:50 +0530

    Changes to the code.

--- Basics/HandlingImages.py
+++ Basics/HandlingImages.py
@@ -4,9 +4,9 @@ import numpy as np
 def main():
 	
 	# reading the image
-	image = cv2.imread('TestImages/peppers_color.tif',0)
+	image = cv2.imread('Images/ImageSet-2/lena_color_256.tif',1)
 	# Displaying the image
-	cv2.imshow('Doremon',image)
+	cv2.imshow('Lena',image)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
